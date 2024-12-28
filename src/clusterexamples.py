9e813d0b91a2bd749e857255a6211d0c36464a39
Lance Hendricks <67243394+lanceHEN@users.noreply.github.com>
2024-12-27 17:46:21 -0800

    Update clusterexamples.py indexing

--- src/clusterexamples.py
+++ src/clusterexamples.py
@@ -5,6 +5,6 @@ pos = input('Enter position: ')
 
 clusterer = Clusterer(year, pos)
 clusters = clusterer.make_clusters()
-for i in range(len(clusters)):
-    print("Cluster {}: ".format(i + 1))
+for i in range(1, len(clusters) + 1):
+    print("Cluster {}: ".format(i))
     print(clusters[i])
